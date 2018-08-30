5348058d01574ef2efb059dd39d9a8e6f3cb81ee
Camille Hall <camille@creativeambition.net>
2018-08-30 00:23:33 -0700

    testing that at least 1 article entry loads successful

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -85,7 +85,7 @@ describe("The menu", function(){
   }); //closes The menu
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+describe("Initial Entries", function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
@@ -93,10 +93,29 @@ describe("The menu", function(){
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
+    beforeEach(function(done) {
+        setTimeout(function() {
+          value = 0;
+          done();
+        }, 1);
+    });
+
+    it('should output entry from loadFeed', function(done){
+        value++;
+        expect($('.feed').children.length).toBeGreaterThan(0);
+        done();
+    });
+
+
     /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    it('should load content of selected RSS feed', function(done){
+
+        done();
+    })
+}) // close initial entries
 }());
